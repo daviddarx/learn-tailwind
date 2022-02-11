673fad8b95583544dbd4ec68cfd0f6febaec8fb9
David Darx <david.darx@liip.ch>
2022-02-11 13:56:17 +0100

    feat(config) extends default theme
    and create backup of default config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#ff6363",
+                secondary: {
+                    100: "#e2e2d5",
+                    200: "#888883"
+                }
+            },
+            fontSize: {
+                "10xl": ["10rem", { lineHeight: "1" }]
+            }
+        }
+    },
+    plugins: []
+};

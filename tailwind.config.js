6a9024e25a44f39b50280f2bcb5555669886aa28
David Darx <david.darx@liip.ch>
2022-02-11 14:53:39 +0100

    feat(setup) changes tab indent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                primary: "#ff6363",
-                secondary: {
-                    100: "#e2e2d5",
-                    200: "#888883"
-                }
-            },
-            fontFamily: {
-                main: ["Nunito"]
-            },
-            fontSize: {
-                "10xl": ["10rem", { lineHeight: "1" }]
-            }
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883"
         }
-    },
-    plugins: []
+      },
+      fontFamily: {
+        main: ["Nunito"]
+      },
+      fontSize: {
+        "10xl": ["10rem", { lineHeight: "1" }]
+      }
+    }
+  },
+  plugins: []
 };
